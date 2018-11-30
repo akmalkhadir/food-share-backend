27bc970097455f8cc93da4d56ae40187a0460305
Irfan Suleman <ivsuleman@gmail.com>
2018-11-30 15:39:58 +0000

    db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Consumer.create(username: 'irfan', password: 'irfan')
+
+Producer.create(username: 'akmal', password: 'akmal')
+
+Food.create(name:'rice', description:'white rice', location:'131 Finsbury Pavement, London EC2A 1NT, UK', quantity:5, vegetarian:true, vegan:true, gluten_free:false, nuts:false, seafood: false, halal:true,kosher:false, start_time:'2018-11-01 21:00', end_time:'2018-12-03 21:00')
