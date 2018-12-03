4069764665e05db50c4ab14d9559f97c4f889f09
Irfan Suleman <ivsuleman@gmail.com>
2018-12-03 11:21:19 +0000

    added serialization

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,4 +1,5 @@
 class Consumer < ApplicationRecord
     has_secure_password
+    has_many :reservations
     has_many :foods, through: :reservations
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ApplicationRecord
     belongs_to :producer
+    has_many :reservations
     has_many :consumers, through: :reservations
 end
