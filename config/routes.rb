eca916ced8d305535d67fd5f831086f929d3e01c
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-04 17:45:58 +0000

    Food POST Done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :consumers, only: [:index]
     resources :producers, only: [:index]
-    resources :foods, only: [:index]
+    resources :foods, only: [:index, :create]
     resources :reservations, only: [:index, :create]
   end
 
