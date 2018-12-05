1fdcc24cba772693921ad8d954ae895bf7a46189
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-05 14:25:54 +0000

    decrease quantity

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,11 @@
 class Reservation < ApplicationRecord
     belongs_to :food
     belongs_to :consumer
+    after_create :reduce_quantity
+
+    def reduce_quantity
+        @food = Food.find(self.food_id)
+        @food.quantity -= 1
+        @food.save
+    end
 end
