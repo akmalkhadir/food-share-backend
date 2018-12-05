4b11db12d21006b811e53ecca5f52ddb42ffaced
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-05 14:05:00 +0000

    show single consumer

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'reservations/create'
 
   namespace :v1 do
-    resources :consumers, only: [:index]
+    resources :consumers, only: [:index, :show]
     resources :producers, only: [:index]
     resources :foods, only: [:index, :create]
     resources :reservations, only: [:index, :create]
